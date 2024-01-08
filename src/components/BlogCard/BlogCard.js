41b81e93bdb7597131db8b0c540dcef390d14ffc
mjaumi <mjaumi2864@gmail.com>
2024-01-09 02:24:08 +0600

    fake blogs data creation done

---
+++ src/components/BlogCard/BlogCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogCard = () => {
+    // rendering blog card component here
+    return (
+        <div>
+            <p>blog card</p>
+        </div>
+    );
+};
+
+export default BlogCard;
--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,18 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PageTitle from '../../components/PageTItle/PageTitle';
 import PageTopBanner from '../../components/PageTopBanner/PageTopBanner';
+import BlogCard from '../../components/BlogCard/BlogCard';
 
 const Blogs = () => {
+    // declaration of react hooks here
+    const [blogs, setBlogs] = useState([]);
+
     //scroll to the top on render
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
+    useEffect(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(blogs => setBlogs(blogs));
+    }, []);
+
+    console.log(blogs);
+
     // rendering blogs page here
     return (
         <section>
             <PageTitle title={'Blogs'} description={'Blogs page of SNL International. Here, user can get to know about all the blogs of SNL International.'} canonical={process.env.NODE_ENV === 'development' ? 'http://localhost:3000/blogs' : 'http://snlbd.com/blogs'} />
+
             <PageTopBanner bgImage={'bg-banner-4-img'} titleText={'Our Blogs'} />
+
+            <div className='w-3/4 grid grid-cols-4 mx-auto py-20'>
+                {
+                    blogs.map(blog => <BlogCard key={blog.id} />)
+                }
+            </div>
         </section>
     );
 };
